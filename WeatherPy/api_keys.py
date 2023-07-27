492b365ffd4a6e7e8353af3422683ab5921e880d
nancyrsantiago <115676220+nancyrsantiago@users.noreply.github.com>
2023-07-27 18:39:46 -0400

    Update api_keys.py

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,6 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "29f49f6cb95fd152263c63ca851ee227"
-
+weather_api_key = 
 # Geoapify API Key
-geoapify_key = "03427a8db0ff47b5818b7ac9669f498c"
+geoapify_key = 
 
